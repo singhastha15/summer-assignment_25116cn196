181a7f72ebf9cb5e5bb6e2fab9e05b49b4f602a8
singhastha15 <singhasthakumari2005@gmail.com>
2026-06-19 20:47:33 +0530

    Add files via upload

---
+++ Day18/binarysearch.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int binarysearch(int arr[], intsize, int target) {
+    int left = 0;
+    int right = size - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == target) {
+            return mid; // Target found
+        }
+        if (arr[mid] < target) {
+            left = mid + 1; // Search in the right half
+        } else {
+            right = mid - 1; // Search in the left half
+        }
+    }
+    return -1; // Target not found
+}
